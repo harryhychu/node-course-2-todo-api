3856fc48f835595b6cf15165e757c478e3a6309f
harry chu <harryhychu@gmail.com>
2018-06-26 11:24:43 +0800

    setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongodb_url = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
+const mongodb_url = process.env.MONGODB_URI;
 
 mongoose.Promise = global.Promise;
 mongoose.connect(mongodb_url);
